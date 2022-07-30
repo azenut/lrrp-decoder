93fabba44e771575ab046ce1d67ceff64af3485a
azenut <nikedex@gmail.com>
2022-07-30 17:32:45 +0200

    map added

--- lrrp-decoder.py
+++ lrrp-decoder.py
@@ -1,20 +1,46 @@
+import folium
+
 def lat_decode(lat):
     lat = ''.join(lat.upper().split())
     lat = int(lat, 16)
     lat = lat * (180/0xFFFFFFFF)
     return lat
 
-def long_decode(long):
-    long = ''.join(long.upper().split())
-    long = int(long, 16)
-    long = long * (360/0xFFFFFFFF)
-    return long
+def long_decode(lng):
+    lng = ''.join(lng.upper().split())
+    lng = int(lng, 16)
+    lng = lng * (360/0xFFFFFFFF)
+    return lng
 
-def get_gmaps_link(lat, long):
-    return f"https://www.google.com/maps/search/{lat_decode(lat)}+{long_decode(long)}"
+# generate a link to see the coordinates on Google Maps
+def get_gmaps_link(x, y):
+    return f"https://www.google.com/maps/search/{lat_decode(x)}+{long_decode(y)}"
 
-x = "44 66 3A 10"
-y = "1D 44 0B C1"
+# extract latitudes and longitudes from radio output
+def parse_dsd_file(pos):
+    with open("./parsing/logfile.txt", "r") as file:
+        
+        lines = file.readlines()
+        data = []
+        
+        for line in lines: # filter the data bytes only
+            if "Rate" in line:
+                splt_line = [x for x in line.split() if len(x) == 2 and x != "BS"]
+                data.append(splt_line)
+        data = [x for x in data if len(x) == 9]
 
-print("40.xxxxx 16.xxxxx -- planned location lat and long")
-print(f"{lat_decode(x)} {long_decode(y)}\n" + get_gmaps_link(x, y))
+        for x in data:
+            coord = [lat_decode("".join(x[-8:-4])), long_decode("".join(x[-4:]))]
+            pos.append(coord)
+        
+
+# MAIN                   
+
+positions = []
+
+parse_dsd_file(positions)
+
+m = folium.Map(location=[40.828301550404994, 16.553154320679383], tiles="OpenStreetMap", zoom_start=14)
+for x in positions:
+    folium.Marker(x).add_to(m)
+m.save("map.html")
